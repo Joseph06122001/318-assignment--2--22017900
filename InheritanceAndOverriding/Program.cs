72e5395851561518ecbbe89d2efebbc13c3901d6
Joseph06122001 <jboateng241@st.ug.edu.gh>
2026-09-02 21:25:09 +0000

    Demonstrate inheritance and method overriding

---
+++ InheritanceAndOverriding/Program.cs
@@ -0,0 +1,31 @@
+﻿Animal animal = new Animal();
+Animal dog = new Dog();
+Animal cat = new Cat();
+
+animal.MakeSound();
+dog.MakeSound();
+cat.MakeSound();
+
+class Animal
+{
+	public virtual void MakeSound()
+	{
+		Console.WriteLine("Some generic sound");
+	}
+}
+
+class Dog : Animal
+{
+	public override void MakeSound()
+	{
+		Console.WriteLine("Bark");
+	}
+}
+
+class Cat : Animal
+{
+	public override void MakeSound()
+	{
+		Console.WriteLine("Meow");
+	}
+}
